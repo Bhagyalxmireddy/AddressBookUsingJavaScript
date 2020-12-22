0e83eb50e912ee2ed0f01db532dc4d6bfaa4d928
Bhagi <puppyreddy1997@gmail.com>
2020-12-22 18:08:30 +0530

    [Bhagyalaxmi]ADDED:Creating The AddressBook

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,13 @@
 console.log("welcome to AddressBook Program");
+class Contact{
+    constructor(...params){
+        this.firstname=params[0];
+        this.lastname=params[1];
+        this.address=params[2];
+        this.city=params[3];
+        this.state=params[4];
+        this.zip=params[5];
+        this.phonenumber=params[6];
+        this.email=params[7];
+    }
+}
